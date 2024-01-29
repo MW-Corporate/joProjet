cc84cce08a54363841f8030a7f02e98bff98fd5b
MW-Corporate <151166113+MW-Corporate@users.noreply.github.com>
2024-01-29 14:46:20 +0000

    Add files via upload

---
+++ joProjet/src/main/java/io/jpabuddy/spring/demo/joProjet/JoProjetApplication.java
@@ -0,0 +1,13 @@
+package io.jpabuddy.spring.demo.joProjet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoProjetApplication.class, args);
+	}
+
+}
